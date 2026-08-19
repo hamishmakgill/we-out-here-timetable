f7e1aa6764cff59a8d6b32d49c5f8a99383231b8
hamishmakgill <67957621+hamishmakgill@users.noreply.github.com>
2026-08-19 07:54:05 +0100

    We Out Here 2026 timetable

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = "woh26-v1";
+
+self.addEventListener("install", (e) => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(["./", "./index.html"]))
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(e.request)
+        .then((res) => {
+          const copy = res.clone();
+          caches.open(CACHE).then((cache) => cache.put(e.request, copy));
+          return res;
+        })
+        .catch(() => caches.match("./index.html"));
+    })
+  );
+});
